eb68f6a64f25a7db6e2345ba67a53421be7d94f0
mepmepp <213323465+mepmepp@users.noreply.github.com>
2025-10-20 22:21:34 +0200

    class dresseur initialization

---
+++ models/dresseur.ts
@@ -0,0 +1,28 @@
+import { Pokemon } from './pokemon.js';
+
+export class Dresseur {
+    name: string;
+    level: number;
+    experience: number;
+    pokemons: Pokemon[];
+    
+    constructor(name: string) {
+        this.name = name;
+        this.level = 1;
+        this.experience = 0;
+        this.pokemons = [];
+    }
+
+    gainExp() {
+
+    }
+
+    addPokemon() {
+
+    }
+
+    treatPokemons() {
+
+    }
+
+}
