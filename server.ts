b11aa03400f0dcc1defe0d4d628cf5a03c7ee5fa
mepmepp <213323465+mepmepp@users.noreply.github.com>
2025-10-29 09:07:45 +0100

    server  update

--- server.ts
+++ server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { randomChallenge } from './controller/random_challenge.ts';
+import { deterministicChallenge } from './controller/deterministic_challenge.ts';
+import { firstArena } from './controller/first_arena.ts';
+import { secondArena } from './controller/second_arena.ts';
 dotenv.config();
 
 const app = express();
@@ -7,6 +11,15 @@ const port = process.env.PORT;
 console.log(port);
 
 app.use(express.json());
+
+const main = () => {
+    app.get('/random-challenge', (request, response) => randomChallenge(request, response));
+    app.get('deterministic-challenge', (request, response) => deterministicChallenge(request, response));
+    app.get('/first-arena', (request, response) => firstArena(request, response));
+    app.get('/second-arena', (request, response) => secondArena(request, response));
+}
+
+main();
 
 app.listen(port, () => {
     console.log(`DB-Test listening on port ${port}`);
