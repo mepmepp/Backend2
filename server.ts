83e1a70002ce51a15d10b6030fd44efda4611523
mepmepp <213323465+mepmepp@users.noreply.github.com>
2025-12-10 08:49:19 +0100

    use swagger in server

--- server.ts
+++ server.ts
@@ -1,19 +1,25 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import swaggerUi from "swagger-ui-express";
+
 import { randomChallenge } from './controller/random_challenge.ts';
 import { deterministicChallenge } from './controller/deterministic_challenge.ts';
 import { firstArena } from './controller/first_arena.ts';
 import { secondArena } from './controller/second_arena.ts';
+import { swaggerSpec } from './middlewares/swagger.ts';
+
+import type { Request, Response } from 'express'; 
+
 dotenv.config();
 
-const app = express();
+export const app = express();
 const port = process.env.PORT;
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 
 const main = () => {
-    app.get('/', (request, response) => response.send(
+    app.get('/', (request, response) => response.status(200).send(
         `<h1>Welcome to Pokemon Challenges!</h1>
         <h2>Routes:</h2>
         <h3>Random Challenges:</h3>
@@ -37,6 +43,4 @@ const main = () => {
 
 main();
 
-app.listen(port, () => {
-    console.log(`DB-Test listening on port ${port}`);
-});
+app.listen(port, () => console.log(`Server listening on port ${port}`));
